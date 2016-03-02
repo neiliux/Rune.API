85da2d6d6bacadbaca0b1f25d7a11f6f07d376dc
Sam Noedel <sam.noedel@gmail.com>
2016-03-01 21:45:38 -0800

    Use restify. Move handlers into directory & use registration style

--- src/index.js
+++ src/index.js
@@ -1,44 +1,27 @@
 'use strict';
 
-var server = require('express')();
-var bodyParser = require('body-parser');
-var request = require('request');
-var expressJwt = require('express-jwt');
-var config = require('./config');
-var searchHandler = require('./search');
-var imageHandler = require('./imageHandler');
-var createDeckHandler = require('./createDeckHandler');
-var decksHandler = require('./decksHandler');
-var authenticateHandler = require('./authenticateHandler');
-var mtgJson = require('../mtgjson.json');
-
+let config = require('../config');
+let restify = require('restify');
+let jwt = require('restify-jwt');
+let mtgJson = require('../mtgjson.json');
 let bunyan = require('bunyan');
 let log = bunyan.createLogger({ name: 'rune' });
 let HandlerRegistrar = require('./handlerRegistrar');
-let requestLogger = require('./request-logger');
-
 
 log.info('Starting up server...');
 
-server.use(requestLogger);
-server.use(bodyParser.json());
-server.use('/api', expressJwt({ secret: config.jwtSecret }));
-
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", config.allowedHeaders);
-    next();
-});
+let server = restify.createServer();
+server.use(jwt({ secret: config.jwtSecret }));
+server.use(restify.CORS());
+server.use(restify.bodyParser());
 
 let registrar = new HandlerRegistrar(server);
 registrar.register('/collections', require('./handlers/collection'));
+registrar.register('/search', require('./handlers/search')(mtgJson));
+registrar.register('/image/:id', require('./handlers/image'));
+registrar.register('/decks', require('./handlers/decks'));
+registrar.register('/auth', require('./handlers/auth'));
 
-searchHandler.init(mtgJson, server);
-authenticateHandler.init(server);
-imageHandler.init(server);
-decksHandler.init(server);
-createDeckHandler.init(server);
-
-server.listen(8080, function() {
-    log.info('Listening on port 8080');
+server.listen(config.port, function() {
+    log.info(`Listening on port ${config.port}`);
 });
