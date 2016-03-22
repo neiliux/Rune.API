49cfb24137dff8a63bf3de28f32398a1daebccc8
Sam Noedel <sam.noedel@gmail.com>
2016-03-21 19:55:17 -0700

    Add more consistent logging

--- src/handler-registrar.js
+++ src/handler-registrar.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const bunyan = require('bunyan');
+
+let log = bunyan.createLogger({ name: 'rune' });
+
 function HandlerRegistrar(server) {
   if (!server) {
     throw new Error('Server is required');
@@ -9,6 +13,7 @@ function HandlerRegistrar(server) {
 
 HandlerRegistrar.prototype.register = function(uri, methods) {
   Object.keys(methods).map((method) => {
+    log.info({ method: method, path: uri }, 'Registering route')
     let callback = methods[method];
     this.server[method](uri, callback);
   });
