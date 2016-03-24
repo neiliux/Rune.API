fd8a433ad45ae5755bc97978370d95d4e39064d5
Sam Noedel <sam.noedel@gmail.com>
2016-03-23 21:02:24 -0700

    Allow searching of set-specific cards

--- src/handlers/card.js
+++ src/handlers/card.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const cardRepo = require('../repositories/card'),
+  setRepo = require('../repositories/set'),
   restify = require('restify');
 
 module.exports = {
   get: (req, res, next) => {
-    return cardRepo.get(req.params['id'], req.query['set'])
+    let cardName = req.params['id'],
+      set = req.query['set'];
+
+    let cardSource = set
+      ? setRepo.getCard(set, cardName)
+      : cardRepo.get(cardName);
+
+    return cardSource
       .do(card => {
         if (!card) {
           throw new restify.errors.NotFoundError();
--- src/repositories/card.js
+++ src/repositories/card.js
@@ -9,18 +9,15 @@ module.exports = {
   bulkFind: cardNames => {
     return MongoClient.connect().flatMap(bulkFind(cardNames));
   },
-  get: (cardName, set) => {
-    return MongoClient.connect().flatMap(find(cardName, set));
+  get: cardName => {
+    return MongoClient.connect().flatMap(find(cardName));
   }
 };
 
-function find(cardName, set) {
+function find(cardName) {
   return db => {
     let fields = { _id: 0 };
     let criteria = { name: cardName };
-    if (set) {
-      criteria['printings'] = { $elemMatch: { $eq: set } }
-    }
     let query = db.collection(CARD_COLLECTION).find(criteria, fields);
     return Observable.fromNodeCallback(query.next, query)();
   };
